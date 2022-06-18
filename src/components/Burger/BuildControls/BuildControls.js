3305c373a47c91cfcbf13b705573f090ffeec11b
Neeraj S D <neerajsd04@gmail.com>
2022-06-18 11:28:03 +0530

    backdrop and modal added

---
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import classes from './BuildControls.css'
+import BuildControl from './BuildControl/BuildControl';
+
+const controls = [
+    { label: 'Salad', type: 'salad' },
+    { label: 'Bacon', type: 'bacon' },
+    { label: 'Cheese', type: 'cheese' },
+    { label: 'Meat', type: 'meat' },
+]
+
+
+const BuildControls = (props) => {
+    return (
+        <div className={classes.BuildControls}>
+            <div>Current price: {props.price}</div>
+
+            {controls.map(ctrl => {
+                console.log(ctrl.label)
+                return <BuildControl key={ctrl.label} label={ctrl.label} type={ctrl.type} increase={props.increase} decrease={props.decrease} />
+            })}
+
+            <button
+                className={classes.OrderButton}
+                disabled={props.price <= 4}
+                onClick={props.ordered}
+            >ORDER NOW</button>
+
+        </div>
+    );
+}
+
+export default BuildControls;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,6 +2,16 @@ import React, { Component } from "react";
 
 import Aux from "../../hoc/ReactAux";
 import Burger from "../../components/Burger/Burger";
+import BuildControls from "../../components/Burger/BuildControls/BuildControls";
+import Modal from "../../components/UI/Modal/Modal";
+import OrderSummary from "../../components/Burger/OrderSummary/OrderSummary";
+
+const INGREDIENT_PRICES = {
+    salad: 0.5,
+    cheese: 0.4,
+    meat: 1.3,
+    bacon: 0.7,
+}
 
 class BurgerBuilder extends Component {
 
@@ -11,14 +21,63 @@ class BurgerBuilder extends Component {
             bacon: 0,
             cheese: 0,
             meat: 0,
+        },
+        totalPrice: 4,
+        purchasing: false,
+    }
+
+    addIngredientHandler = (type) => {
+        const oldCount = this.state.ingredients[type]
+        const updatedCount = oldCount + 1
+        const updatedIngredient = {
+            ...this.state.ingredients
         }
+        updatedIngredient[type] = updatedCount
+        const updatedPrice = this.state.totalPrice + INGREDIENT_PRICES[type]
+
+        this.setState({
+            ingredients: updatedIngredient,
+            totalPrice: updatedPrice,
+        })
+    }
+
+    decreaseIngredientHandler = (type) => {
+        const oldCount = this.state.ingredients[type]
+        const updatedCount = oldCount - 1
+
+        if (updatedCount < 0) return
+
+        const updatedIngredient = {
+            ...this.state.ingredients
+        }
+        updatedIngredient[type] = updatedCount
+        const updatedPrice = this.state.totalPrice - INGREDIENT_PRICES[type]
+
+        this.setState({
+            ingredients: updatedIngredient,
+            totalPrice: updatedPrice,
+        })
+    }
+
+    purchaseHandler = () => {
+        this.setState({
+            purchasing: true,
+        })
     }
 
     render() {
         return (
             <Aux>
+                <Modal show={this.state.purchasing}>
+                    <OrderSummary ingredients={this.state.ingredients} />
+                </Modal>
                 <Burger ingredients={this.state.ingredients} />
-                <div>Build Controls</div>
+                <BuildControls
+                    increase={this.addIngredientHandler}
+                    decrease={this.decreaseIngredientHandler}
+                    price={this.state.totalPrice}
+                    ordered={this.purchaseHandler}
+                />
             </Aux>
         )
     }
